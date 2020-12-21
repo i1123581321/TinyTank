afb6edc1850661752b03c00cebd3a583d4bcdba3
Qian <gokinjolno112358@gmail.com>
2020-12-21 10:43:57 +0800

    Initial Commit

---
+++ TinyTank/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  TinyTank
+//
+//  Created by 钱正轩 on 2020/12/21.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
